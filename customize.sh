ef18b0c0a28545556fd8b1f5a5a6f63f58104f8c
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2020-04-25 21:28:42 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,7 +7,7 @@ mkdir -p `dirname $MODPATH$CFG`
 ui_print "- Migrating $CFG"
 [[ -f /sbin/.magisk/mirror$SELECTPATH ]] && cp -af /sbin/.magisk/mirror$SELECTPATH $MODPATH$SELECTPATH || cp -af $SELECTPATH $MODPATH$SELECTPATH
 ui_print "- Starting modifiy"
-sed -i '/gChannelBondingMode24GHz=/d;/gChannelBondingMode5GHz=/d;/gForce1x1Exception=/d;s/^END$/gChannelBondingMode24GHz=1\ngChannelBondingMode5GHz=1\ngForce1x1Exception=0\nEND/g' $MODPATH$SELECTPATH
+sed -i '/gRegulatoryChangeCountry=0/d;/gCountryCodePriority=1/d;s/^END$/gRegulatoryChangeCountry=1\ngCountryCodePriority=0\nEND/g' $MODPATH$SELECTPATH
 }
 done
 [[ -z $SELECTPATH ]] && abort "- Installation FAILED. Your device didn't support WCNSS_qcom_cfg.ini." || { mkdir -p $MODPATH/system; mv -f $MODPATH/vendor $MODPATH/system/vendor;}
